9b24202680bc576cc0d0d87b43ba3bafcfd73ff6
Marius Hagelin <marius.hagelin@gmail.com>
2022-01-18 11:04:36 +0100

    Implemented Screens and Details

---
+++ lib/widgets/meal_item.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+
+import '../screens/meal_detail_screen.dart';
+import '../models/meal.dart';
+
+class MealItem extends StatelessWidget {
+  //const MealItem({Key? key}) : super(key: key);
+  final String id;
+  final String title;
+  final String imageUrl;
+  final int duration;
+  final Complexity complexity;
+  final Affordability affordability;
+
+  MealItem({
+    required this.id,
+    required this.title,
+    required this.imageUrl,
+    required this.duration,
+    required this.complexity,
+    required this.affordability,
+  });
+
+  String get complexityText {
+    switch (complexity) {
+      case Complexity.Simple:
+        return 'Simple';
+      case Complexity.Challenging:
+        return 'Challenging';
+      case Complexity.Hard:
+        return 'Hard';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  String get affordabilityText {
+    switch (affordability) {
+      case Affordability.Affordable:
+        return 'Affordable';
+      case Affordability.Pricey:
+        return 'Pricey';
+      case Affordability.Luxurious:
+        return 'Expensive';
+      default:
+        return 'Unknown';
+    }
+  }
+
+  void selectMeal(BuildContext context) {
+    Navigator.of(context).pushNamed(
+      MealDetailScreen.routeName,
+      arguments: id,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () => selectMeal(context),
+      child: Card(
+        //RoundedRectangleBorder can be used on Card
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        elevation: 4,
+        margin: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                //RoundedRectangleBorder can not be used on Stack, can use ClipRRect to get same effect
+                //Bilde av Mat
+                ClipRRect(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                  ),
+                  child: Image.network(
+                    imageUrl,
+                    height: 250,
+                    width: double.infinity,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 0,
+                  child: Container(
+                    width: 280,
+                    color: Colors.black54,
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: const TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            // Info under bilde:
+            Padding(
+              padding: const EdgeInsets.all(20),
+              child: Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    //Tid
+                    Row(children: [
+                      const Icon(Icons.schedule),
+                      const SizedBox(width: 6),
+                      Text('$duration min'),
+                    ]),
+                    //Vanskelighetsgrad
+                    Row(children: [
+                      const Icon(Icons.work),
+                      const SizedBox(width: 6),
+                      Text(complexityText),
+                    ]),
+                    //Pris
+                    Row(children: [
+                      const Icon(Icons.attach_money),
+                      const SizedBox(width: 6),
+                      Text(affordabilityText),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
